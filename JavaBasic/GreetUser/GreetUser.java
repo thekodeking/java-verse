c67bde04fb84d6f7c4876a335cc3f5c7d8e159d1
= <=>
2020-11-06 21:00:10 +0530

    Added Directories for each program to add readme to all the programs. Added GreetUser.java program

---
+++ JavaBasic/GreetUser/GreetUser.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; // Scanner class is requied to get user inputs 
+
+public class GreetUser {
+    public static void main(String[] args) {
+        if (args.length > 0){ // checks for commandline arguments...
+            greet(args[0]); // calls greet("") method
+        }
+        else { // if user didn't give any commandline arguments ...
+            System.out.print( "Enter your Name:" ); // Prints the text: Enter your Name
+            Scanner scan = new Scanner ( System.in ); // scan is an object of Scanner
+            String name = scan.nextLine (); // name gets the user input
+            scan.close (); // closes the "System.in" input stream
+            greet(name); // calls greet("") method
+        }
+    }
+
+    public static void greet(String username){
+        System.out.println ( "Welcome to your workstation " + username + "." ); // prints the statement given
+    }
+}
---
+++ JavaBasic/HelloWorld/README.md
@@ -0,0 +1,4 @@
+# Hello World Program
+
+A very basic program that every developer/programmer learns to write. A Hello World Program gives you a clarity on the basic outline of a language and the way to program the language.
+Below here is a java version of Hello World program.
